287f4b7bf02e1443bc9b59296bc9b841a6fef3a4
Kei-K23 <keiksl2301@gmail.com>
2024-11-21 15:38:42 +0630

    feat(api): add booking model

---
+++ RoomBookAPI/Models/Booking.cs
@@ -0,0 +1,15 @@
+namespace RoomBookAPI.Models
+{
+    public class Booking
+    {
+        public Guid Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+        public int RoomId { get; set; }
+        public Room Room { get; set; } = null!;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Status { get; set; } = "Booked"; // Booked, Confirm, Cancelled
+    }
+}
